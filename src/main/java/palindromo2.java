a96b4be29898f2639d84bcae93c5da537ab2b870
Gustavo <g.calderon05@ufromail.cl>
2022-04-13 07:38:17 -0400

    Segunda clase

--- src/main/java/palindromo2.java
+++ src/main/java/palindromo2.java
@@ -1,2 +1,18 @@
 public class palindromo2 {
+    public palindromo2() {
+    }
+
+    public boolean espalindromo(String cadena) {
+        boolean valor = true;
+        StringBuilder cadena2 = new StringBuilder();
+
+        for(int x = 0; x < cadena.length(); ++x) {
+            if (cadena.charAt(x) != ' ') {
+                cadena2.append(cadena.charAt(x));
+            }
+        }
+
+        cadena = cadena2.toString();
+
+
 }
